b88be2c567e5453f68d4c23117e5de02e86a4f56
Bhanu Rakshita Paul <88148227+bhanuRakshita@users.noreply.github.com>
2024-03-22 14:14:32 -0400

    Adding Favourites Functionality (Part 2)

---
+++ pages/favourites.jsx
@@ -0,0 +1,33 @@
+import { useAtom } from "jotai";
+import { Row, Col } from "react-bootstrap";
+
+import { favouritesAtom } from "@/store";
+import ArtworkCard from "@/src/components/ArtworkCard";
+const Favourites = () => {
+
+    const [favouritesList, setFavouritesList] = useAtom(favouritesAtom);
+
+    if(favouritesList) {
+        return (
+            <>
+              <Row className="gy-4">
+                {favouritesList.length > 0 &&
+                  favouritesList.map((currObjID) => {
+                    return (
+                      <Col lg={3} key={currObjID}>
+                        <ArtworkCard objectID={currObjID} />
+                      </Col>
+                    );
+                  })}
+                {favouritesList.length == 0 && <h4>Nothing Here: Try adding something</h4>}
+              </Row>
+      
+              
+            </>
+          );
+        }
+    };
+
+
+
+export default Favourites;
