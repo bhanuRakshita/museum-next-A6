14b337c0d8a6717fb4c2ae18c628653a68b1cec4
Bhanu Rakshita Paul <88148227+bhanuRakshita@users.noreply.github.com>
2024-03-22 13:21:17 -0400

    Automatically Hiding the Expanded Navbar (Mobile / Small Screens)

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -13,8 +13,10 @@ import { useRouter } from 'next/router';
 function MainNav() {
   const searchRouter = useRouter();
   const [searchField, setSearchField] = useState('');
+  const [isExpanded, setIsExpanded] = useState(false);
   const submitHandler = (e) => {
     e.preventDefault();
+    isExpanded(false);
     searchRouter.push(`/artwork?title=true&q=${encodeURIComponent(searchField)}`);
   }
 
@@ -22,18 +24,22 @@ function MainNav() {
     setSearchField(e.target.value);
   }
 
+  const toggleIsExpanded = () => {
+    setIsExpanded(!isExpanded);
+  }
+
   return (
-    <Navbar expand="lg" className="bg-body-tertiary fixed-top">
+    <Navbar expanded={isExpanded} expand="lg" className="bg-body-tertiary fixed-top">
       <Container fluid>
         <Navbar.Brand href="#">Bhanu Rakshita Paul</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Toggle aria-controls="navbarScroll" onClick={toggleIsExpanded}/>
         <Navbar.Collapse id="navbarScroll">
         <Nav className="me-auto">
               <Link href="/" passHref legacyBehavior>
-                <Nav.Link>Home</Nav.Link>
+                <Nav.Link onClick={toggleIsExpanded}>Home</Nav.Link>
               </Link>
-              <Link href="/search" passHref legacyBehavior>
-                <Nav.Link>Advanced Search</Nav.Link>
+              <Link href="/search" passHref legacyBehavior >
+                <Nav.Link onClick={toggleIsExpanded}>Advanced Search</Nav.Link>
               </Link>
             </Nav>
           <Form className="d-flex" onSubmit={submitHandler}>
